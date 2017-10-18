2e172cda2ea8c84961f9c8a4ac955cee1b290efb
Kali Donovan <kali@deviantech.com>
2017-10-18 10:59:46 +0200

    Cleaner UX

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -1,3 +1,7 @@
+function addFields(component, container) {
+  container = container || $('body')
+  container.find('a.add_fields[data-association="'+component+'"]').first().click()
+}
 function initSelect2() {
   $('.select2').select2({
       theme: "bootstrap",
@@ -6,7 +10,16 @@ function initSelect2() {
 }
 
 $(document).ready(initSelect2)
+$(document).ready(function(){
+  if ( $('.nested-fields .step').length == 0) {
+    addFields('step')
+  }
+})
 
 $(document).on('cocoon:after-insert', function(e, insertedItem) {
   initSelect2()
+
+  if (insertedItem.children().first().hasClass('step')) {
+    addFields('component', insertedItem)
+  }
 });
